d14ed3f218ba6ab9a782d6cbab5b22033b53965e
codelixir <pahulpreet.singh@research.iiit.ac.in>
2021-06-06 17:49:12 +0530

    Created and migrated Note model

--- noughtpad/noughtpad_app/models.py
+++ noughtpad/noughtpad_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title + " | " + str(self.author)
+
+    # def __init__(self):
+    #     ...
