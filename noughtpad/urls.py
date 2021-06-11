75821bdd34044ec31b4cbd7b5f71fb2bb5d8d1d9
codelixir <pahulpreet.singh@research.iiit.ac.in>
2021-06-11 23:25:04 +0530

    Initialised Auth App

--- noughtpad/urls.py
+++ noughtpad/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('noughtpad_app.urls'))
+    path('', include('noughtpad_app.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
+    path('auth/', include('authpad_app.urls'))
 ]
