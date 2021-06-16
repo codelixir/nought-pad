9e28eb744b152168604cdde9ae5d993c4a28033f
codelixir <pahulpreet.singh@research.iiit.ac.in>
2021-06-16 23:05:09 +0530

    added Comments model

--- noughtpad_app/admin.py
+++ noughtpad_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Note, Category
+from .models import Note, Category, Comment
 
 admin.site.register(Note)
 admin.site.register(Category)
+admin.site.register(Comment)
--- noughtpad_app/models.py
+++ noughtpad_app/models.py
@@ -1,3 +1,4 @@
+from django.db.models.deletion import CASCADE
 from django.urls import reverse
 from django.db import models
 from django.contrib.auth.models import User
@@ -24,6 +25,18 @@ class Note(models.Model):
 
     def total_likes(self):
         return self.likes.count()
+
+
+class Comment(models.Model):
+    'Model for a comment on a note'
+    note = models.ForeignKey(
+        Note, related_name="comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.note.title + " | " + self.name
 
 
 class Category(models.Model):
