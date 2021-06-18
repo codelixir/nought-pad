2955173ef481e19d436c28f1e040c95489e7c67f
codelixir <pahulpreet.singh@research.iiit.ac.in>
2021-06-18 10:31:11 +0530

    Create model Profile

--- noughtpad_app/admin.py
+++ noughtpad_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Note, Category, Comment
+from .models import Note, Category, Comment, Profile
 
 admin.site.register(Note)
+admin.site.register(Profile)
 admin.site.register(Category)
 admin.site.register(Comment)
--- noughtpad_app/models.py
+++ noughtpad_app/models.py
@@ -1,7 +1,9 @@
 from django.db.models.deletion import CASCADE
+from django.db.models.fields.related import OneToOneField
 from django.urls import reverse
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 from ckeditor.fields import RichTextField
 
 
@@ -27,11 +29,28 @@ class Note(models.Model):
         return self.likes.count()
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    about = models.TextField()
+    profile_pic = models.ImageField(
+        null=True, blank=True, upload_to="user_content/profile_images/")
+    website = models.CharField(max_length=255)
+    facebook = models.CharField(max_length=255)
+    twitter = models.CharField(max_length=255)
+    instagram = models.CharField(max_length=255)
+    github = models.CharField(max_length=255)
+    linkedin = models.CharField(max_length=255)
+    link = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.user.username
+
+
 class Comment(models.Model):
     'Model for a comment on a note'
     note = models.ForeignKey(
         Note, related_name="comments", on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
@@ -45,3 +64,6 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def __init__(self):
+        self.slug = slugify(self.name)
