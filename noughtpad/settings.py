02d8781c638055365b02b5cb885e59b6ecc0a741
codelixir <pahulpreet.singh@research.iiit.ac.in>
2021-07-01 00:13:04 +0530

    fixed import error

--- noughtpad/settings.py
+++ noughtpad/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import keys
+import noughtpad.keys as keys
 import os
 from pathlib import Path
 
